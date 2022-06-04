86a070f5d0e8f71413cf883787e4f1e18ca97564
ubuntu <franztejada@hotmail.com>
2022-06-03 20:45:47 -0400

    2022-06-03

---
+++ app/Http/Livewire/Categories.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+use App\Models\Category;
+use Illuminate\Support\Facades\Storage;
+use Livewire\WithFileUploads;
+use Livewire\WithPagination;
+
+class Categories extends Component
+{
+    use WithFileUploads;
+    use WithPagination;
+
+    public $name, $search, $image, $selectedId, $pageTitle, $componentName;
+    private $pagination = 5;
+
+    public function mount()
+    {
+        $this->pageTitle = 'Listado';
+        $this->componentName = 'Categorías';
+    }
+
+    public function render()
+    {
+        $data = Category::all();
+
+        return view('livewire.category.categories', ['categories' => $data])
+            ->extends('layouts.theme.app')
+            ->section('content');
+    }
+}
